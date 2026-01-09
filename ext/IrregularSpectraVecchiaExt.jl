e251e85462bb351048403d06570153b7d77a26aa
Chris Geoga <geoga@wisc.edu>
2026-01-09 13:33:57 -0600

    Update the Vecchia.jl extension for the preconditioner to use the new syntax. I'll need to change the internals of this library to solve the weights in F64 instead of C64 (which is slightly complicated because they're being solved with the matrix square root) to allow for general permutations. But it doesn't work better than the sparse one with NearestNeighbors.jl in its current form anyway, so I wouldn't say it is a high priority.

--- ext/IrregularSpectraVecchiaExt.jl
+++ ext/IrregularSpectraVecchiaExt.jl
@@ -8,45 +8,17 @@ module IrregularSpectraVecchiaExt
 
   using IrregularSpectra: KrylovSolver, VecchiaPreconditioner, gen_kernel
 
-  struct SparseInvCholesky
-    Us::UpperTriangular{Float64, SparseMatrixCSC{Float64, Int64}}
-    perm::Vector{Int64}
-    iperm::Vector{Int64}
-  end
-
-  LinearAlgebra.ishermitian(scp::SparseInvCholesky) = true
-  LinearAlgebra.issymmetric(scp::SparseInvCholesky) = true
-  Base.eltype(scp::SparseInvCholesky)  = Float64
-  Base.size(scp::SparseInvCholesky)    = size(scp.Us)
-  Base.size(scp::SparseInvCholesky, j) = size(scp.Us, j)
-  function LinearAlgebra.mul!(buf, scp::SparseInvCholesky, x)
-    tmp1 = scp.Us'*x
-    mul!(buf, scp.Us, tmp1)
-  end
-
   function IrregularSpectra.krylov_preconditioner!(pts_sa, Ω, solver::KrylovSolver{VecchiaPreconditioner,K}; 
                                                   verbose=false) where{K}
     kernel  = gen_kernel(solver, pts_sa, Ω)
     _kernel = (x, y, _) -> kernel(x, y) + Float64(x==y)*solver.perturbation
-    multithread   = Threads.nthreads() > 1
-    blas_nthreads = BLAS.get_num_threads()
-    if multithread
-      @info "Temporarily setting BLAS threads to 1 for parallel preconditioner assembly..." maxlog=1
-      BLAS.set_num_threads(1)
-    end
     pre_time = @elapsed begin
-      cfg = knnconfig(Float64.(eachindex(pts_sa)), pts_sa, 
-                      solver.preconditioner.ncond, 
-                      _kernel; randomize=false)
-      p   = vec(Int64.(reduce(vcat, cfg.data)))
-      permute!(pts_sa, p) # really should permute the solution with invperm...
-      Us  = sparse(Vecchia.rchol(cfg, Float64[]; use_tiles=true, issue_warning=false))
-    end
-    if multithread
-      BLAS.set_num_threads(blas_nthreads)
+      cond   = KNNConditioning(solver.preconditioner.ncond)
+      approx = VecchiaApproximation(pts_sa, _kernel; conditioning=cond, ordering=NoPermutation())
+      pre    = rchol_preconditioner(approx, Float64[]).U
     end
     verbose && @printf "Preconditioner assembly time: %1.3fs\n" pre_time
-    (false, SparseInvCholesky(Us, p, invperm(p)))
+    (false, pre)
   end
 
 end
